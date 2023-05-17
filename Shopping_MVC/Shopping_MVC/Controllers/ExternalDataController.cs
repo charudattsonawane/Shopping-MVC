005cf102e6332c655137a1ef234d22b9b33e14f6
Sonawane <2053909@cognizant.com>
2023-05-17 12:25:21 +0530

    commited

---
+++ Shopping_MVC/Shopping_MVC/Controllers/ExternalDataController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping_MVC.Controllers
+{
+    public class ExternalDataController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
